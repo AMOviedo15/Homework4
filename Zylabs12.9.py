e85684ba9af616ac5ef7a917139861eb1c26d4db
AMOviedo15 <77904077+AMOviedo15@users.noreply.github.com>
2021-05-05 01:47:14 -0500

    Update Zylabs12.9.py

--- Zylabs12.9.py
+++ Zylabs12.9.py
@@ -1,1 +1,13 @@
 # Aaron Oviedo 1990958
+
+
+name_age = input().split()
+name = name_age[0]
+while name != '-1':
+    try:
+        age = int(name_age[1]) + 1
+    except Exception as age_ex:
+        age = 0
+    print(name, age)
+    name_age = input().split()
+    name = name_age[0]
